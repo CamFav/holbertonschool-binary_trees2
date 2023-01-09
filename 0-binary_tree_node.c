7a809175617ef9aa207319aae796effffcc97bc0
Meashyshy <camillefvrl@gmail.com>
2023-01-09 02:08:00 -0800

    first commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates a binary tree node.
+ * @parent: pointer to parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: Always 0.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	if (!parent || !value)
+		return;
+}
