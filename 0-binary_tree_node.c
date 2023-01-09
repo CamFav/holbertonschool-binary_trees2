a99c52c90de4aa3346454214aa94280e4e6a2e27
Meashyshy <camillefvrl@gmail.com>
2023-01-09 02:46:17 -0800

    Fixed syntaxe error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 	if (value < parent->n)
 	{
-		insert(value, parent->value);
+		insert(value, parent->left);
 	}
 	else if (value > parent->n)
 	{
