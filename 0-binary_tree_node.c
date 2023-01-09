12043d533538799a7600886a2478154545febea6
Meashyshy <camillefvrl@gmail.com>
2023-01-09 03:00:34 -0800

    Deleted insert function

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,21 +10,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tempNode = NULL;
 
-	if (!parent)
-	{
-		tempNode = malloc(sizeof(binary_tree_t));
-		tempNode->left = tempNode->right = NULL;
-		tempNode->n = value;
-		parent = tempNode;
+	if (!tempNode)
 		return (NULL);
-	}
-	if (value < parent->n)
-	{
-		insert(parent->left, value);
-	}
-	else if (value > parent->n)
-	{
-		insert(parent->right, value);
-	}
-	return (NULL);
+
+	tempNode = malloc(sizeof(binary_tree_t));
+	tempNode->left = tempNode->right = NULL;
+	tempNode->n = value;
+	tempNode->parent = parent;
+
+	return (tempNode);
 }
