c732c7b6eec80e2e72be8de0b98291f2311de16b
Meashyshy <camillefvrl@gmail.com>
2023-01-10 07:42:41 -0800

    Commit

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,33 +1,16 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - function that inserts a node as the left-child
+ * binary_tree_is_leaf - function that inserts a node as the left-child
  * of another node.
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store the new node
+ * @node: pointer to node to check
  *
  * Return: Always 0.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	binary_tree_t *newNode;
+	if (node)
+		return (0);
 
-	if (!parent)
-		return (NULL);
-
-	newNode = malloc(sizeof(binary_tree_t));
-
-	if (!newNode)
-		return (NULL);
-
-	newNode->right = newNode->left = NULL;
-	newNode->n = value;
-	newNode->parent = parent;
-
-	if (parent->right)
-	{
-		newNode->right = parent->right;
-	}
-	parent->right = newNode;
-
-	return (newNode);
+	if (node->left || node->right)
+		return (1);
 }
