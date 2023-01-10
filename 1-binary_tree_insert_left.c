f63e636d6538a9b9d6fbb38ba5a69e90b19b0856
Meashyshy <camillefvrl@gmail.com>
2023-01-10 06:07:08 -0800

    Added NULL declaration

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,9 +15,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newNode->parent = newNode->left = NULL;
+	newNode->right = NULL;
 	newNode->n = value;
 	newNode->left = parent;
 
 	return (newNode);
-	free(newNode);
 }
