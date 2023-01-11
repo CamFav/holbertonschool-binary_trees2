8b72efe8fd5081d8874dc7fb66b5b2971234868d
Meashyshy <camillefvrl@gmail.com>
2023-01-11 02:29:59 -0800

    NULL

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,7 +10,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if (node->parent->left || node->parent->right)
+	if (node->left || node->right || node->parent != NULL:)
 	{
 		return (0);
 	}
