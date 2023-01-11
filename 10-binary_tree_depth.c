e1b3d3b5178bd08f66ae63a3f111594b92d2dfc7
Meashyshy <camillefvrl@gmail.com>
2023-01-11 05:58:27 -0800

    Task 10 - Function that Measures the Depth of a Node

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - function that measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to meaasure the height.
+ *
+ * Return: Always 0.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	left_height = binary_tree_depth(tree->left);
+	right_height = binary_tree_depth(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+
+	return (right_height + 1);
+}
