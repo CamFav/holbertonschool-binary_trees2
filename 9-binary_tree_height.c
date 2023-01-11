86649f557dd7b4ec1b16965de763b8f403863d8b
Meashyshy <camillefvrl@gmail.com>
2023-01-11 05:45:12 -0800

    commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,17 +10,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int left_height;
 	int right_height;
 
-	if (!tree)
-		return (0);
-
-	if (!tree->left && !tree->right)
+	if (!tree || !tree->left && !tree->right)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		return (left_height + 0);
+		return (left_height + 1);
 
-	return (right_height + 0);
+	return (right_height + 1);
 }
