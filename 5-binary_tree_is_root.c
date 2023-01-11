47d1c2192449d73d7dd17e6e0a6518bbce5731e8
Meashyshy <camillefvrl@gmail.com>
2023-01-11 02:27:06 -0800

    First commit

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - function that checks if a given node is a root.
+ * @node: pointer to node to check
+ *
+ * Return: Always 0.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (node->parent->left || node->parent->right)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
