67ba47497b92f1ce82434fc352737ff96e134fcb
Meashyshy <camillefvrl@gmail.com>
2023-01-11 04:10:14 -0800

    Added recursive

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,9 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (!tree)
-	{
 		return (0);
+	else
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+
+		if (left_height >= right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
 	}
-	return (0);
 }
