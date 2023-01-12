f62e87fbe7518b7f074c181530ea0c40dfc50316
Meashyshy <camillefvrl@gmail.com>
2023-01-12 02:26:19 -0800

    Commit

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,19 +7,15 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int left_depth;
-	int right_depth;
+	size_t depth = 0;
 
 	if (!tree)
 		return (0);
 
-	while (tree->left && tree->right)
+	while (tree->parent)
 	{
-		left_depth = binary_tree_depth(tree->left);
-		right_depth = binary_tree_depth(tree->right);
-
-		if (left_depth > right_depth)
-			return (left_depth + 1);
+		depth++;
+		binary_tree_depth(tree->parent);
 	}
-	return (right_depth + 1);
+	return (depth);
 }
