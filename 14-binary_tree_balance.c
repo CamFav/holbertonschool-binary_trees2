66994ad9e49643cf2d3c83ea661b1706cca38528
Meashyshy <camillefvrl@gmail.com>
2023-01-12 04:24:22 -0800

    Commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,11 +14,31 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = binary_tree_balance(tree->left);
-	right_height = binary_tree_balance(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to meaasure the height.
+ *
+ * Return: Always 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	left_height = 1 + binary_tree_height(tree->left);
+	right_height = 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		return (left_height + 1);
+		return (left_height);
 
-	return (right_height + 1);
+	return (right_height);
 }
